4b112621281c4f5bfc57a5997207182bc04bc996
Himaja-02 <himajakommireddy2@gmail.com>
2019-12-18 18:53:00 +0530

    New Change

--- src/testng/Employee.java
+++ src/testng/Employee.java
@@ -1,11 +1,19 @@
 package testng;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class Employee {
+	
 @Test
 public void sys(){
+	WebDriver d=null;
+	d.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
 	System.out.println("Himaja");
+	
+	System.out.println("Goutham");
 }
 
 }
