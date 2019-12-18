40a632001ef338bf4395f859e6a601edd56a33de
Himaja-02 <himajakommireddy2@gmail.com>
2019-12-18 19:02:38 +0530

    new change to master by hima2

--- src/testng/Employee.java
+++ src/testng/Employee.java
@@ -1,11 +1,17 @@
 package testng;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class Employee {
 @Test
-public void sys(){
-	System.out.println("Himaja");
+public void him(){
+	WebDriver d=null;
+	WebDriverWait w=new WebDriverWait(d, 20);
+	WebElement h=d.findElement(By.id("email"));
 }
 
 }
